1d443cde5e65781200cca5340d4a97ffafe1bb12
saptakds <saptakds@gmail.com>
2020-09-29 16:47:41 +0530

    Added Check who won 21 times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,24 +1,33 @@
 #!/bin/bash -x
 function getCoinFace(){
-	if [ $1 -eq 1 ]
-	then
-		echo "Heads"
-	else
-		echo "Tails"
-	fi
+    if [ $1 -eq 1 ]
+    then
+        echo "Heads"
+    else
+        echo "Tails"
+    fi
 }
 heads=0
 tails=0
-for i in `seq 1 10`
+while [ $heads -lt 21 -a $tails -lt 21 ]
 do
-	toss=$((1+$RANDOM%2))
-	winner=(`getCoinFace $toss`)
-	if [ $winner = "Heads" ]
-	then
-		((heads++))
-	else
-		((tails++))
-	fi
+    toss=$((1+$RANDOM%2))
+    winner=(`getCoinFace $toss`)
+    if [ $winner = "Heads" ]
+    then
+        ((heads++))
+    else
+        ((tails++))
+    fi
 done
+if [ $heads -eq $tails ]
+then
+    echo "Tie"
+elif [ $heads -lt $tails ]
+then
+    echo "Tails won by $(($tails-$heads))"
+else
+    echo "Heads won by $(($heads-$tails))"
+fi
 echo "Heads: $heads"
 echo "Tails: $tails"
