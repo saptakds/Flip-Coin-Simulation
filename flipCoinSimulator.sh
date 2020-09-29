b9a48cd6840fca3bfbfb2ffe5c8dc43512913c34
saptakds <saptakds@gmail.com>
2020-09-29 17:06:14 +0530

    Added Continuing tosses in case of tie to lead by min 2

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -22,8 +22,21 @@ do
 done
 if [ $heads -eq $tails ]
 then
-    echo "Tie"
-elif [ $heads -lt $tails ]
+    diff=`echo $(($heads-$tails)) | tr -d -`
+	while [ $diff -ne 2 ]
+	do
+		toss=$((1+$RANDOM%2))
+    	winner=(`getCoinFace $toss`)
+    	if [ $winner = "Heads" ]
+    	then
+        	((heads++))
+    	else
+        	((tails++))
+    	fi
+		diff=`echo $(($heads-$tails)) | tr -d -`
+	done
+fi
+if [ $heads -lt $tails ]
 then
     echo "Tails won by $(($tails-$heads))"
 else
